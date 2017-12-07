dd7b16b1bc818f35b3d3ea831041cf5fa25092cd
Jamie Quinn <j.quinn.1@research.gla.ac.uk>
2017-12-07 18:21:59 +0000

    Moved print variables script

--- tools/print_variables.py
+++ tools/print_variables.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""Prints mode, temperature, vorticity and stream function"""
 
 import argparse
 import numpy as np
 
 def main():
+    """main function"""
     parser = argparse.ArgumentParser(description='Print variable contents')
 
     parser.add_argument('filenames', help='files to open', nargs='+')
@@ -15,12 +17,15 @@ def main():
                         help='number of gridpoints in simulation')
     args = parser.parse_args()
 
-    Nn = int(args.n_modes)
-    Nz = int(args.n_gridpoints)
+    n_modes = args.n_modes
+    n_gridpoints = args.n_modes
     data = np.fromfile(args.filenames[0], dtype=np.dtype(np.double))
-    temp = np.transpose(data[:Nn*Nz].reshape(Nn, Nz))
-    omega = np.transpose(data[Nn*Nz:2*Nn*Nz].reshape(Nn, Nz))
-    psi = np.transpose(data[2*Nn*Nz:3*Nn*Nz].reshape(Nn, Nz))
+    temp = np.transpose(data[:n_modes*n_gridpoints]\
+                        .reshape(n_modes, n_gridpoints))
+    omega = np.transpose(data[n_modes*n_gridpoints:2*n_modes*n_gridpoints]\
+                         .reshape(n_modes, n_gridpoints))
+    psi = np.transpose(data[2*n_modes*n_gridpoints:3*n_modes*n_gridpoints]\
+                       .reshape(n_modes, n_gridpoints))
 
     for i in range(args.max_print_mode+1):
         print i, "%.5E" % temp[32, i], "%.5E" % omega[32, i], "%.5E" % psi[32, i]
