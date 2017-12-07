4f2ef9ec8cbfbd5d0b15dad80f2d04a721487829
Jamie Quinn <j.quinn.1@research.gla.ac.uk>
2017-12-07 18:56:07 +0000

    Moved full test into folder

--- test/integration_test.sh
+++ test/integration_test.sh
@@ -7,7 +7,7 @@ a=3
 T=5e-2
 S=0.01
 Ra=1e6
-folder=benchmark
+folder=test/benchmark
 ICs=initial_conditions/ICn1nZ101nN51
 mkdir -p $folder
 rm -f $folder/*
@@ -16,7 +16,7 @@ make nonlinear
 echo "==================== Starting program"
 build/exe -nZ $nZ -nN $nN -dt $dt -Ra $Ra -Pr $Pr -a $a -T $T -S $S -i $ICs -o $folder/ | tee $folder/log
 echo "==================== Comparing results"
-comparison_results=$(python tools/printVariables.py benchmark/vars5.dat --max_print_mode 20 --n_modes 51 --n_gridpoints 101 | column -t | diff -q - data/benchmark_t5.txt)
+comparison_results=$(python tools/print_variables.py test/benchmark/vars5.dat --max_print_mode 20 --n_modes 51 --n_gridpoints 101 | column -t | diff -q - test/benchmark_t5.txt)
 if [ -n "$comparison_results" ]; then
   echo "Simulation returned different results!"
   exit -1
